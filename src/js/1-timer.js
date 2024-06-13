825a2e64f76691e59a79375d19a130a1a846c285
Nazar Nazarko <starmok20@gmail.com>
2024-06-13 11:34:53 +0300

    add files

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,84 +1,77 @@
 import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
+
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const refs = {
-    datePicker: document.querySelector('#datetime-picker'),
-    btnStart: document.querySelector('[data-start]'),
-    days: document.querySelector('[data-days]'),
-    hours: document.querySelector('[data-hours]'),
-    minutes: document.querySelector('[data-minutes]'),
-  seconds: document.querySelector('[data-seconds]'),
-    timer: document.querySelector('.timer'),
-};
-let intervalId;
-let initTime;
+let userSelectedDate;
+let countdownInterval;
 
- // деактивація кнопки старт
-refs.btnStart.disabled = true; 
 const options = {
-  enableTime: true,
-  time_24hr: true,
-  defaultDate: new Date(),
-  minuteIncrement: 1,
-  onClose(selectedDates) {
-    const userSelectedDate = selectedDates[0];
-    initTime = userSelectedDate;
-    if (userSelectedDate <= Date.now()) {
-      iziToast.show({  message: 'Please choose a date in the future'});
-  refs.btnStart.disabled = true; 
-  };
-  },
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+        userSelectedDate = selectedDates[0];
+        const countdownInterval = userSelectedDate - new Date();
+
+        if (countdownInterval < 0) {
+               iziToast.error({
+        color: 'red',
+        position: 'topRight',
+        message: 'Please choose a date in the future',
+      });
+            
+            startBtn.disabled = true;
+            inputTime.disabled = false;
+        } else {
+            startBtn.disabled = false;
+            inputTime.disabled = true;
+        }
+    },
 };
-flatpickr('#datetime-picker', options);
 
+const flatpickrInstance = flatpickr('#datetime-picker', options);
+const inputTime = document.querySelector('#datetime-picker');
+const startBtn = document.querySelector('[data-start]');
+const timeValues = document.querySelectorAll('.value');
+
+startBtn.disabled = true;
+
+startBtn.addEventListener('click', () => {
+  countdownInterval = setInterval(() => {
+    const timeInterval = userSelectedDate - new Date();
     
-refs.datePicker.addEventListener('click', () => {
-    refs.btnStart.disabled = false;
-  });
+    if (timeInterval < 0) {
+      clearInterval(countdownInterval);
+      startBtn.disabled = true;
+      inputTime.disabled = false;
+      return;
+    }
 
-
-refs.btnStart.addEventListener('click', () => {
-       intervalId = setInterval(() => {
-        const currentTime = Date.now();
-        const diff = initTime - currentTime;
-         const time = convertMs(diff);
-         const str = getTime(time);
-         refs.timer.textContent = str;
-         refs.btnStart.disabled = true; 
-        // console.log(str);
-       }, 1000);
-  setTimeout(() => {
-  clearInterval(intervalId)
-}, initTime - Date.now());
+    const { days, hours, minutes, seconds } = convertMs(timeInterval);
+    updateTimerDisplay({ days, hours, minutes, seconds });
+  }, 1000);
 });
 
-
-function getTime ({ days, hours, minutes, seconds }) {
-  days = days.toString().padStart(2,  '0');
-    hours = hours.toString().padStart(2,  '0');
-    minutes = minutes.toString().padStart(2,  '0');
-  seconds = seconds.toString().padStart(2,  '0');
-  return `${days}:${hours}:${minutes}:${seconds}`
-};
-
-//***** ms - різниця між кінцевою і поточною датою в мілісекундах
 function convertMs(ms) {
-  // Number of milliseconds per unit of time
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Remaining days
   const days = Math.floor(ms / day);
-  // Remaining hours
   const hours = Math.floor((ms % day) / hour);
-  // Remaining minutes
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Remaining seconds
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
+}
+
+function updateTimerDisplay({ days, hours, minutes, seconds }) {
+  timeValues[0].textContent = String(days).padStart(2, '0');
+  timeValues[1].textContent = String(hours).padStart(2, '0');
+  timeValues[2].textContent = String(minutes).padStart(2, '0');
+  timeValues[3].textContent = String(seconds).padStart(2, '0');
 }
